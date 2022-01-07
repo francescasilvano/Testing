a558a0f90b69d98681ce55a4ab225ffc89a5b90a
francescasilvano <77408604+francescasilvano@users.noreply.github.com>
2022-01-07 17:55:12 +0100

    Update phase_shifter.vhd

--- phase_shifter.vhd
+++ phase_shifter.vhd
@@ -14,14 +14,10 @@ entity phase_shifter is
 end phase_shifter;
 
 architecture beh of phase_shifter is
-signal r_reg: std_logic_vector(N downto 0);
 begin
-	behaviour: process(clk)
-	begin
-    if clk'event and clk = '1' then 
-        r_reg <= input_p(0) & input_p(N downto 1);
-	end if;
-	end process;
-	output_p<= r_reg XOR input_p;
+
+	output_p<= (input_p(0) & input_p(N downto 1)) XOR input_p;
+
+	
               
 end beh;
